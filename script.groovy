ed7bfc1666662c424f8ef7c08dba034c04f0aeba
somtoc71-collab <somtoc71@gmail.com>
2026-07-23 00:40:19 +0100

    Enhance buildimage to push Docker image
    Added Docker push command to buildimage function.

--- script.groovy
+++ script.groovy
@@ -5,8 +5,10 @@ def buildjar() {
 
 def buildimage() {
      echo 'building the image ..'
-     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])            sh 'docker build -t bamzy14/my-repo:jma-2.0 .'
-       sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
+    sh 'docker build -t bamzy14/my-repo:jma-2.0 .'
+    sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+    sh 'docker push bamzy14/my-repo:jma-2.0'
 }
 
 def deploy() {
