584c40b329522af47d75ff756b760b0cce28b7bc
somtoc71-collab <somtoc71@gmail.com>
2026-07-24 11:59:31 +0100

    Refactor App class to remove Spring Boot components
    Removed Spring Boot annotations and hello endpoint.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,22 +1,10 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
+/**
+ * Hello world!
+ */
 public class App {
-
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        System.out.println("Hello World!");
     }
-
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, Jenkins Pipeline!";
-    }
-}
 }
